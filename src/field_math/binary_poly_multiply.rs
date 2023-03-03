4ab7124a6394c67d3850aa299d4e804593f00b73
Chen Xiao <chenxiao770117@gmail.com>
2023-03-03 11:38:58 +0800

    update

--- src/field_math/binary_poly_multiply.rs
+++ src/field_math/binary_poly_multiply.rs
@@ -1,8 +1,37 @@
+use crate::field_math::pitem::Pitem;
+use crate::field_math::poly_to_items::poly_to_items;
+
 pub fn binary_poly_multiply(poly1: &str, poly2: &str) -> String {
     poly_multiply_basic(poly1, poly2, "+",  "*")
 }
 
-fn poly_multiply_basic(poly1: &str, poly2: &str, add_op: &str, multiply_op: &str) -> String {
+fn poly_multiply_basic(poly_multiplicand: &str, poly_multiplier: &str, add_op: &str, multiply_op: &str) -> String {
+    let multiplicand_pitems = poly_to_items(poly_multiplicand);
+    let multiplier_pitems = poly_to_items(poly_multiplier);
+    
+    let mut result_pitems: Vec<Pitem> = Vec::new();
+
+    println!("{:?}, {:?}", multiplicand_pitems, multiplier_pitems);
+
+    for multiplicand_pitem in &multiplicand_pitems {
+        for multiplier_pitem in &multiplier_pitems {
+            result_pitems.push(
+                Pitem {
+                    x_index: match add_op {
+                        "+" => multiplier_pitem.x_index + multiplicand_pitem.x_index,
+                        _ => 0,
+                    },
+                    coe: match multiply_op {
+                        "*" => multiplier_pitem.x_index * multiplicand_pitem.x_index,
+                        _ => 0,
+                    }
+                }
+            );
+        }
+    }
+
+    println!("{:?}", result_pitems);
+
     String::from("")
 }
 
