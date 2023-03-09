7d39a25f7ba1c3f9e14d680d6dc132fd0c3e2a82
Chen Xiao <chenxiao770117@gmail.com>
2023-03-09 11:14:51 +0800

    update

--- src/field_math/galios_poly_multiply.rs
+++ src/field_math/galios_poly_multiply.rs
@@ -1,7 +1,48 @@
 use crate::field_math::galios_context::GaliosContext;
+use crate::field_math::pitem::Pitem;
+use crate::field_math::poly_to_items::poly_to_items;
+use crate::field_math::items_to_poly::items_to_poly;
+use crate::field_math::galios_poly_add::galios_poly_add;
+use crate::field_math::galios_num_multiply::galios_num_multiply;
 
-pub fn galios_poly_multiply(polys: Vec<&str>, gs: &GaliosConext) -> String {
-    String::from("")
+pub fn galios_poly_multiply(polys: Vec<&str>, gs: &GaliosContext) -> String {
+    let mut polys_iter = polys.iter();
+
+    let mut poly_multiplicand = String::from(*polys_iter.next().unwrap());
+
+    loop {
+        match polys_iter.next() {
+            Some(poly_multiplier) => {
+                let multiplicand_pitems = poly_to_items(&poly_multiplicand);
+                let multiplier_pitems = poly_to_items(poly_multiplier);
+                
+                let mut result_polys: Vec<String> = Vec::new();
+
+                for multiplicand_pitem in &multiplicand_pitems {
+
+                    let mut result_pitems = Vec::new();
+
+                    for multiplier_pitem in &multiplier_pitems {
+                        result_pitems.push(
+                            Pitem {
+                                x_index: multiplier_pitem.x_index + multiplicand_pitem.x_index,
+                                coe: galios_num_multiply(multiplier_pitem.coe, multiplicand_pitem.coe, &gs),
+                            }
+                        );
+                    }
+
+                    let poly_multiplied = items_to_poly(result_pitems);
+                    
+                    result_polys.push(String::from(&poly_multiplied))
+                }
+                
+                poly_multiplicand = galios_poly_add(result_polys.iter().map(|poly| poly.as_str()).collect());
+                
+                continue;
+            },
+            None => break poly_multiplicand
+        }
+    }
 }
 
 #[cfg(test)]
@@ -17,7 +58,7 @@ mod tests {
         
         assert_eq!("x", galios_poly_multiply(vec!["1", "x"], &gs));
         assert_eq!("x6+x5+x4+x", galios_poly_multiply(vec!["x3+x", "x3+x2+1"], &gs));
-        assert_eq!("x3+x2+x1+1", galios_poly_multiply(vec!["x2+x1+1", "x"], &gs));
+        assert_eq!("x3+x2+x", galios_poly_multiply(vec!["x2+x1+1", "x"], &gs));
         assert_eq!("x4+x3+x2", galios_poly_multiply(vec!["x2+x1+1", "x2"], &gs));
         assert_eq!("x2+3x+2", galios_poly_multiply(vec!["x+1", "x+2"], &gs));
         assert_eq!("x3+7x2+14x+8", galios_poly_multiply(vec!["x2+3x+2", "x+4"], &gs));
