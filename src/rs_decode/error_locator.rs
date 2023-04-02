024b44c0fb7895f6f918c76a40856e534725ad0b
Chen Xiao <chenxiao770117@gmail.com>
2023-04-02 20:18:26 +0800

    update

--- src/rs_decode/error_locator.rs
+++ src/rs_decode/error_locator.rs
@@ -1,9 +1,31 @@
 use crate::field_math::galios_context::new_gs_from_value;
 use crate::field_math::galios_context::GaliosContext;
 use crate::field_math::galios_num_multiply::galios_num_multiply;
+use crate::field_math::items_to_poly::items_to_poly;
+use crate::field_math::pitem::Pitem;
 
-pub fn error_locator(syndromes: Vec<u32>, error_length: u32, gs: &GaliosContext) -> Result<(String, String), bool> {
+pub fn error_locator(
+    syndromes: Vec<u32>,
+    error_length: u32,
+    gs: &GaliosContext,
+) -> Result<(String, String), bool> {
     println!("syndromes: {:?}, error_length: {error_length}", syndromes);
+
+    let syndromes_items: Vec<Pitem> = syndromes
+        .iter()
+        .enumerate()
+        .map(|(index, item)| Pitem {
+            x_index: (syndromes.len() - index - 1usize) as u32,
+            coe: *item,
+        })
+        .collect();
+
+    let syndromes_poly = items_to_poly(syndromes_items.clone());
+
+    println!(
+        "syndromes_items: {:?}, syndromes_poly: {syndromes_poly}",
+        syndromes_items
+    );
 
     Ok(("".to_string(), "".to_string()))
 }
